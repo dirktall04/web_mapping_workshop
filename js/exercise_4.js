65be139288fd54a2ab17897b9cf75b13490e892e
Dirk <dirktall04@users.noreply.github.com>
2016-04-25 09:37:48 -0500

    Updated exercise_4.js

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -21,15 +21,5 @@ map.setView([39, -96], 4);
 var dataFileToAdd = 'data/parks.geojson';
 
 var featureLayer = L.mapbox.featureLayer();
-featureLayer.loadURL(dataFileToAdd);
-featureLayer.addTo(map);
-
-featureLayer.on('ready', function() {
-	this.setStyle({
-      "color": "#6583BF",
-      "fillColor": "#6583BF",
-      "weight": .5,
-      "opacity: 0.65
-    });
-  map.fitBounds(featureLayer.getBounds());
-});
+	.loadURL(dataFileToAdd);
+	.addTo(map);
