d5902ddb0da5d82710b6f1bcfd774fa262be1244
Dirk <dirktall04@users.noreply.github.com>
2016-04-25 09:59:59 -0500

    Updated exercise_4.js

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -34,3 +34,9 @@ featureLayer.on('ready', function() {
     });
   map.fitBounds(featureLayer.getBounds());
 });
+
+featurelayer.on('ready', function() {
+ 	this.eachLayer(function(layer) {
+    layer.bindPopup('Welcome to ' + layer.feature.properties.LABEL);
+    });
+});
